15142cff2bc99748efebf1f20fa5f159b6361899
IvaRo <ivarosoft@gmail.com>
2018-03-23 16:28:41 +0100

    Update lang.pulse.js
    Add lang NL

--- Plugins_langs/lang.pulse.js
+++ Plugins_langs/lang.pulse.js
@@ -453,6 +453,62 @@ $R.addLang('en', {
 	"widget":"Widget",
 	"widget-html-code":"Codul HTML pt Widget"
 }
-
-
-);
+'nl', {
+// alignment
+	"align": "Uitlijnen",
+	"align-left": "Links uitlijnen",
+	"align-center": "Centreren",
+	"align-right": "Rechts uitlijnen",
+	"align-justify": "Uitvullen",
+// clips
+	"clips": "Clips",
+	"clips-select": "Alstublieft, selecteer een clip",
+// filemanager
+	"choose": "Kies",
+// fontcolor
+	"fontcolor": "Tekstkleur",
+    "text": "Tekst",
+    "highlight": "Achtergrondkleur",
+// fontfamily
+	"fontfamily": "Doopvont",
+    "remove-font-family":  "Verwijder lettertypefamilie",
+// fontsize
+	"size": "Grootte",
+    "remove-size":  "Verwijder lettergrootte",
+// fullscreen
+	"fullscreen": "Volledig scherm",
+// imagemanager
+	"choose": "Kies",
+// inlinestyle
+	"style": "Stijl",
+// properties
+	"properties": "Eigenschappen",
+// specialchars
+	"specialchars": "Speciale tekens",
+// table
+	"table": "Tabel",
+	"insert-table": "Tabel invoegen",
+	"insert-row-above": "Rij hierboven invoegen",
+	"insert-row-below": "Rij hieronder invoegen",
+	"insert-column-left": "Kolom links toevoegen",
+	"insert-column-right": "Kolom rechts toevoegen",
+	"add-head": "Titel toevoegen",
+	"delete-head": "Titel verwijderen",
+	"delete-column": "Verwijder kolom",
+	"delete-row": "Verwijder rij",
+	"delete-table": "Verwijder tabel",
+// textdirection
+	"change-text-direction": "RTL-LTR",
+	"left-to-right": "Links naar rechts",
+	"right-to-left": "Rechts naar links",
+// variable
+	"change": "Veranderen",
+    "variable": "Variabele",
+    "variable-select": "Alstublieft, selecteer een variabele",	
+// video	
+	"video": "Video",
+    "video-html-code": "Video embed code of Youtube/Vimeo Link",
+// widget
+	"widget": "Widget",
+    "widget-html-code": "Widget HTML-code"
+});
