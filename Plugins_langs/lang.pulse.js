62afd8c0393c6fd8fe586f6ccaca2fcd5af3647f
IvaRo <ivarosoft@gmail.com>
2018-03-23 13:24:47 +0100

    Language translation for PulseCMS
    Language translation for PulseCMS
    Submitted translation: EN, DE, JA, CA, CS, HU, PL, RO

---
+++ Plugins_langs/lang.pulse.js
@@ -0,0 +1,458 @@
+$R.addLang('en', {
+// alignment
+	"align": "Align",
+	"align-left": "Align Left",
+	"align-center": "Align Center",
+	"align-right": "Align Right",
+	"align-justify": "Align Justify",
+// clips
+	"clips": "Clips",
+	"clips-select": "Please, select a clip",
+// filemanager
+	"choose": "Choose",
+// fontcolor
+	"fontcolor": "Text Color",
+    "text": "Text",
+    "highlight": "Highlight",
+// fontfamily
+	"fontfamily": "Font",
+    "remove-font-family":  "Remove Font Family",
+// fontsize
+	"size": "Size",
+    "remove-size":  "Remove Font Size",
+// fullscreen
+	"fullscreen": "Fullscreen",
+// imagemanager
+	"choose": "Choose",
+// inlinestyle
+	"style": "Style",
+// properties
+	"properties": "Properties",
+// specialchars
+	"specialchars": "Special Characters",
+// table
+	"table": "Table",
+	"insert-table": "Insert table",
+	"insert-row-above": "Insert row above",
+	"insert-row-below": "Insert row below",
+	"insert-column-left": "Insert column left",
+	"insert-column-right": "Insert column right",
+	"add-head": "Add head",
+	"delete-head": "Delete head",
+	"delete-column": "Delete column",
+	"delete-row": "Delete row",
+	"delete-table": "Delete table",
+// textdirection
+	"change-text-direction": "RTL-LTR",
+	"left-to-right": "Left to Right",
+	"right-to-left": "Right to Left",
+// variable
+	"change": "Change",
+    "variable": "Variable",
+    "variable-select": "Please, select a variable",	
+// video	
+	"video": "Video",
+    "video-html-code": "Video Embed Code or Youtube/Vimeo Link",
+// widget
+	"widget": "Widget",
+    "widget-html-code": "Widget HTML Code"
+}
+'sk', {
+// alignment
+	"align": "Zarovnať",
+	"align-left": "Zarovnať vľavo",
+	"align-center": "Zarovnať na stred",
+	"align-right": "Zarovnať vpravo",
+	"align-justify": "Zarovnať do bloku",
+// clips
+	"clips": "Klipy",
+	"clips-select": "Vyberte klip",
+// filemanager
+	"choose": "Vybrať",
+// fontcolor
+	"fontcolor": "Farba písma",
+  "text": "Text",
+  "highlight": "Farba pozadia",
+// fontfamily
+	"fontfamily": "Písma",
+  "remove-font-family":  "Odstrániť formátovanie písma",
+// fontsize
+	"size": "Veľkosť",
+  "remove-size":  "Odstrániť veľkosť písma",
+// fullscreen
+	"fullscreen": "Celá obrazovka",
+// imagemanager
+	"choose": "Vybrať",
+// inlinestyle
+	"style": "Štýl",
+// properties
+	"properties": "Vlastnosti",
+// specialchars
+	"specialchars": "Špeciálne znaky",
+// table
+	"table": "Tabulka",
+	"insert-table": "Vložiť tabulku",
+	"insert-row-above": "Pridať riadok hore",
+	"insert-row-below": "Pridať riadok dole",
+	"insert-column-left": "Pridať stĺpec vľavo",
+	"insert-column-right": "Pridať stľpec vpravo",
+	"add-head": "Pridať záhlavie",
+	"delete-head": "Zmazať záhlavie",
+	"delete-column": "Zmazať stľpec",
+	"delete-row": "Zmazať riadok",
+	"delete-table": "Zmazať tabulku",
+// textdirection
+	"change-text-direction": "RTL-LTR",
+	"left-to-right": "Zľava doprava",
+	"right-to-left": "Zprava doľava",
+// variable
+	"change": "Premenná",
+  "variable": "Premenné",
+  "variable-select": "Vyberte premennú",	
+// video	
+	"video": "Video",
+  "video-html-code": "Kód vloženia videa alebo odkaz Youtube/Vimeo",
+// widget
+	"widget": "Widget",
+  "widget-html-code": "Widget kód HTML"
+}
+'cs', {
+// alignment
+	"align": "Zarovnat",
+	"align-left": "Zarovnat vlevo",
+	"align-center": "Zarovnat na střed",
+	"align-right": "Zarovnat vpravo",
+	"align-justify": "Zarovnat do bloku",
+// clips
+	"clips": "Klipy",
+	"clips-select": "Prosím, vyberte klip",
+// filemanager
+	"choose": "Vybrat",
+// fontcolor
+	"fontcolor": "Barva písma",
+  "text": "Text",
+  "highlight": "Barva pozadí",
+// fontfamily
+	"fontfamily": "Písma",
+  "remove-font-family":  "Odstranit format písma",
+// fontsize
+	"size": "Velikost",
+  "remove-size":  "Odebrat velikost písma",
+// fullscreen
+	"fullscreen": "Celá obrazovka",
+// imagemanager
+	"choose": "Vybrat",
+// inlinestyle
+	"style": "Styl",
+// properties
+	"properties": "Vlastnosti",
+// specialchars
+	"specialchars": "peciální znaky",
+// table
+	"table": "Tabulka",
+	"insert-table": "Vložit tabulku",
+	"insert-row-above": "Přidat řádek nahoru",
+	"insert-row-below": "Přidat řádek dolu",
+	"insert-column-left": "Přidat sloupec vlevo",
+	"insert-column-right": "Přidat sloupec vpravo",
+	"add-head": "Přidat záhlaví",
+	"delete-head": "Smazat záhlaví",
+	"delete-column": "Smazat sloupec",
+	"delete-row": "Smazat řádek",
+	"delete-table": "Smazat tabulku",
+// textdirection
+	"change-text-direction": "RTL-LTR",
+	"left-to-right": "Zleva do prava",
+	"right-to-left": "Zprava doleva",
+// variable
+	"change": "Přeměna",
+  "variable": "Proměnná",
+  "variable-select": "Prosím, vyberte proměnnou",	
+// video	
+	"video": "Video",
+  "video-html-code": "Kód pro vložení videa nebo odkaz Youtube/Vimeo",
+// widget
+	"widget": "Widget",
+  "widget-html-code": "Widget kód HTML"
+}
+'pl', {
+// alignment
+	"align": "Wyrównać",
+	"align-left": "Tekst do lewej",
+	"align-center": "Wyśrodkuj",
+	"align-right": "Tekst do prawej",
+	"align-justify": "Wyjustuj",
+// clips
+	"clips": "Klipy",
+	"clips-select": "Proszę wybrać klip",
+// filemanager
+	"choose": "Wybierz",
+// fontcolor
+	"fontcolor": "Kolor tekstu",
+  "text": "Tekst",
+  "highlight": "Kolor tła",
+// fontfamily
+	"fontfamily": "Czcionki",
+  "remove-font-family":  "Usuń rodzinę czcionek",
+// fontsize
+	"size": "Rozmiar",
+  "remove-size":  "Usuń rozmiar czcionki",
+// fullscreen
+	"fullscreen": "Pełny ekran",
+// imagemanager
+	"choose": "Wybierz",
+// inlinestyle
+	"style": "Styl",
+// properties
+	"properties": "Właściwości",
+// specialchars
+	"specialchars": "Znaki specjalne",
+// table
+	"table": "Tabela",
+	"insert-table": "Wstaw tabele",
+	"insert-row-above": "Dodaj wiersz na górze",
+	"insert-row-below": "Dodaj wiersz na dole",
+	"insert-column-left": "Dodaj kolumnę po lewej",
+	"insert-column-right": "Dodaj kolumnę po prawej",
+	"add-head": "Dodaj nagłówek",
+	"delete-head": "Usuń nagłówek",
+	"delete-column": "Usuń kolumnę",
+	"delete-row": "Usuń wiersz",
+	"delete-table": "Usuń tabele",
+// textdirection
+	"change-text-direction": "RTL-LTR",
+	"left-to-right": "Od lewej do prawą",
+	"right-to-left": "Od prawej do lewej",
+// variable
+	"change": "Zmieniać",
+  "variable": "Zmienna",
+  "variable-select": "Proszę wybrać zmienną",	
+// video	
+	"video": "Wideo",
+  "video-html-code": "Kod do umieszczenia filmu lub link do Youtube/Vimeo",
+// widget
+	"widget": "Widżet",
+  "widget-html-code": "Kod HTML widżetu"
+}
+'hu', {
+// alignment
+	"align": "Igazítsa",
+	"align-left": "Balra igazítás",
+	"align-center": "Középre igazítás",
+	"align-right": "Jobbra igazítás",
+	"align-justify": "Sorkizárt",
+// clips
+	"clips": "Klipek",
+	"clips-select": "Kérjük, válasszon ki egy klipet",
+// filemanager
+	"choose": "Válassz",
+// fontcolor
+	"fontcolor": "Szöveg szine",
+  "text": "Szöveg",
+  "highlight": "Szöveg hátterének szine",
+// fontfamily
+	"fontfamily": "Betűtípusok",
+  "remove-font-family":  "Betűtípusok eltávolítása",
+// fontsize
+	"size": "Betűméret",
+  "remove-size":  "A betűméret eltávolítása",
+// fullscreen
+	"fullscreen": "Teljes képernyős",
+// imagemanager
+	"choose": "Válassz",
+// inlinestyle
+	"style": "Stílus",
+// properties
+	"properties": "Tulajdonságok",
+// specialchars
+	"specialchars": "Speciális karakterek",
+// table
+	"table": "Tábla",
+	"insert-table": "Táblázat beszúrása",
+	"insert-row-above": "Sor beszúrása az aktuális sor fölé",
+	"insert-row-below": "Sor beszúrása ez alá",
+	"insert-column-left": "Oszlop beszúrása ettöl balra",
+	"insert-column-right": "Oszlop hozzáadása ettöl jobbra",
+	"add-head": "Fejrész hozzáadása",
+	"delete-head": "Fejrész törlése",
+	"delete-column": "Oszlop törlése",
+	"delete-row": "Sor törlése",
+	"delete-table": "Táblázat törlése",
+// textdirection
+	"change-text-direction": "RTL-LTR",
+	"left-to-right": "Balról jobbra",
+	"right-to-left": "Jobbról balra",
+// variable
+	"change": "Változás",
+    "variable": "Változó",
+    "variable-select": "Kérjük, válasszon ki egy változót",	
+// video	
+	"video": "Videó",
+    "video-html-code": "Video kód beillesztése vagy Youtube/Vimeo link",
+// widget
+	"widget": "Widget",
+    "widget-html-code": "Widget HTML kód"
+}
+'ja',{ /// ADD JAPANESE VARIATIONS HERE
+// alignment
+	"align": "配置",
+	"align-left": "左揃え",
+	"align-center": "中央揃え",
+	"align-right": "右揃え",
+	"align-justify": "均等分布",	
+// clips
+	"clips":"クリップ",
+	"clips-select":"クリップを選択して下さい。",	
+// file manager
+	"choose": "選択する",	
+// font color
+	"fontcolor":"テキストカラー",
+	"text":"テキスト",
+	"highlight":"ハイライト",	
+// font family
+	"fontfamily":"フォント",
+	"remove-font-family":"フォントを元に戻す",	
+// font size
+	"size":"サイズ",
+	"remove-size":"フォントサイズを元に戻す",	
+// full screen
+	"fullscreen":"フルスクリーン",	
+// image manager
+	"choose":"選択する",	
+// inline
+	"style":"スタイル",	
+// properties
+	"properties":"プロパティ",	
+// table
+	"table":"テーブル",
+	"insert-table":"テーブルを挿入",
+	"insert-row-above":"行の上に挿入",
+	"insert-row-below":"行の下に挿入",
+	"insert-column-left":"左カラムに挿入",
+	"insert-column-right":"右カラムに挿入",
+	"add-head":"ヘッド",
+	"delete-head":"ヘッドを削除",
+	"delete-column":"カラムを削除",
+	"delete-row":"行を削除",
+	"delete-table":"テーブルを削除",	
+// text direction
+	"change-text-direction":"RTL-LTR",
+	"left-to-right":"左から右へ",
+	"right-to-left":"右から左へ",	
+// video
+	"video":"ビデオ",
+	"video-html-code":"エンベットコードまたはYoutube/Vimeoをリンク",	
+// widget
+	"widget":"ウィジェット",
+	"widget-html-code":"ウィジェットのHTML"
+}
+'de',{ /// ADD GERMAN VARIATIONS HERE
+// alignment
+	"align": "Ausrichten",
+	"align-left": "Linksbündig ausrichten",
+	"align-center": "Zentriert ausrichten",
+	"align-right": "Rechtsbündig ausrichten",
+	"align-justify": "Blocksatz",
+// clips
+	"clips":"Clips",
+	"clips-select":"Bitte einen Clip auswählen",
+// file manager
+	"choose": "Auswählen",
+// font color
+	"fontcolor":"Textfarbe",
+	"text":"Text",
+	"highlight":"Hervorheben",
+// font family
+	"fontfamily":"Schriftart",
+	"remove-font-family":"Schriftart entfernen",
+// font size
+	"size":"Größe",
+	"remove-size":"Schriftgröße entfernen",
+// full screen
+	"fullscreen":"Vollbild",
+// image manager
+	"choose":"Auswählen",
+// inline
+	"style":"Stil",
+// properties
+	"properties":"Eigenschaften",
+// table
+	"table":"Tabelle",
+	"insert-table":"Tabelle einfügen",
+	"insert-row-above":"Reihe darüber einfügen",
+	"insert-row-below":"Reihe danach einfügen",
+	"insert-column-left":"Spalte links einfügen",
+	"insert-column-right":"Spalte rechts einfügen",
+	"add-head":"Tabellenkopf hinzufügen",
+	"delete-head":"Tabellenkopf löschen",
+	"delete-column":"Spalte löschen",
+	"delete-row":"Reihe löschen",
+	"delete-table":"Tabelle löschen",
+// text direction
+	"change-text-direction":"RTL-LTR",
+	"left-to-right":"Links nach Rechts",
+	"right-to-left":"Rechts nach Links",
+// video
+	"video":"Video",
+	"video-html-code":"Video Einbettungscode oder Youtube/Vimeo Link",
+// widget
+	"widget":"Widget",
+	"widget-html-code":"Widget HTML Code"
+}
+
+'ro',{
+// alignment
+	"align": "Aliniere",
+	"align-left": "Aliniere Stânga",
+	"align-center": "Aliniere Centru",
+	"align-right": "Aliniere Dreapta",
+	"align-justify": "Aliniere Justify",
+// clips
+	"clips":"Clips",
+	"clips-select":"Selectează un clip",
+// file manager
+	"choose": "Selectează",
+// font color
+	"fontcolor":"Culoare Text",
+	"text":"Text",
+	"highlight":"Highlight",
+// font family
+	"fontfamily":"Font",
+	"remove-font-family":"Înlătură Familia de Fonturi",
+// font size
+	"size":"Dimensiune",
+	"remove-size":"Înlătură Dimensiunea Fontului",
+// full screen
+	"fullscreen":"Ecran Complet",
+// image manager
+	"choose":"Selectează",
+// inline
+	"style":"Stil",
+// properties
+	"properties":"Proprietăți",
+// table
+	"table":"Tabel",
+	"insert-table":"Inserează tabel",
+	"insert-row-above":"Inserează rând deasupra",
+	"insert-row-below":"Inserează rând dedesubt",
+	"insert-column-left":"Inserează coloană în stânga",
+	"insert-column-right":"Inserează coloană în dreapta",
+	"add-head":"Adaugă antet",
+	"delete-head":"Șterge antet",
+	"delete-column":"Șterge coloană",
+	"delete-row":"Șterge rând",
+	"delete-table":"Șterge tabel",
+// text direction
+	"change-text-direction":"DlS-SlD",
+	"left-to-right":"Stânga spre Dreapta",
+	"right-to-left":"Dreapta spre Stânga",
+// video
+	"video":"Video",
+	"video-html-code":"Codul de embed pt video sau link Youtube/Vimeo",
+// widget
+	"widget":"Widget",
+	"widget-html-code":"Codul HTML pt Widget"
+}
+
+
+);
